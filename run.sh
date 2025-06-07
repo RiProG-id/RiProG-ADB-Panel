2257221302fe34333cb1540c29fe3dde64dad250
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2025-06-07 10:46:34 +0800

    Add run.sh script for starting the server

---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/system/bin/sh
+original_dir=$(pwd)
+dir=$(
+	cd "$(dirname "$0")" || exit 1
+	pwd
+)
+if [ ! -f "$dir/server" ]; then
+	arch=$(getprop ro.product.cpu.abi)
+	echo "[INFO] Architecture: $arch"
+	if [ "$arch" = "armeabi-v7a" ] || [ "$arch" = "armv8l" ]; then
+		echo "- Architecture $arch is supported."
+		echo "- Installation continues."
+		cp "$dir/source/arm" "$dir/server"
+	elif [ "$arch" = "arm64-v8a" ]; then
+		echo "- Architecture $arch is supported."
+		echo "- Installation continues."
+		cp "$dir/source/arm64" "$dir/server"
+	else
+		echo "- Architecture $arch is not supported."
+		echo "- Installation aborted."
+		cd "$original_dir" || exit 1
+		exit 1
+	fi
+fi
+parent=$(basename "$dir")
+tmp="/data/local/tmp"
+target="$tmp/$parent"
+pkill -f "$tmp/.*/server"
+rm -rf "$target"
+mkdir -p "$target"
+cp -r "$dir"/* "$target"/
+chmod +x "$target/server"
+cd "$target" || exit 1
+nohup ./server >/dev/null 2>&1 &
+sleep 1
+am start -a android.intent.action.VIEW -d http://localhost:8080
+cd "$original_dir" || exit 1
